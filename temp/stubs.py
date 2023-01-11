e7a1f1e468b766e8c41761d08cc125f6567a93b7
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2023-01-11 19:55:36 +0530

    Added 2 motor use case in ipynb

---
+++ temp/stubs.py
@@ -0,0 +1,24 @@
+# drone class
+## init
+### routine for connection to drone
+
+## exit
+### routine for disconnection from drone
+
+# select frame/type
+
+# set motor directions
+
+# define control allocation matrix
+
+# fetch control allocation matrix
+
+# convert control allocation to actuator effectiveness matrix
+
+# fetch actuator effectiveness matrix
+
+# set desired trajectory setpoints
+
+# convert desired trajectory to desired rpyt
+
+# 
